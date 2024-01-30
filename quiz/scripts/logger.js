fb194fe4953f9ca0e00764c5577570ae3652fa8d
Owen <seltzer.o@northeastern.edu>
2024-01-30 16:58:52 -0500

    Class 013024

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,15 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+
+const logger = {
+	log_it: function() {
+		if (this.errMsg) {
+			console.log(this.errMsg);
+		}
+	}
+}
+
+const logItem = {
+	errMsg: "test error message!"
+}
+
+logger.log_it.call(logItem);
